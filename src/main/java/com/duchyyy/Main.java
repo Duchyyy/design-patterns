77858417ac4366eddd192f47357c4b6f65e636db
Duchyyy <duchoslavandrej@gmail.com>
2021-01-28 13:22:18 +0100

    memento pattern

---
+++ src/main/java/com/duchyyy/Main.java
@@ -0,0 +1,23 @@
+package com.duchyyy;
+
+import com.duchyyy.memento.Editor;
+import com.duchyyy.memento.History;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//  memento pattern
+//        Editor editor = new Editor();
+//        History history = new History();
+//        editor.setContent("text");
+//        history.push(editor.createState());
+//        editor.setContent("check");
+//        history.push(editor.createState());
+//        editor.setContent("box");
+//        editor.restore(history.pop());
+//        System.out.println(editor.getContent());
+
+    }
+
+}
